3d33bdf517ae682521ab0315c1dd58e92dae77fb
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2025-04-06 00:37:02 -0700

    eh just yt for now

--- ocr_api.py
+++ ocr_api.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse
-import pytesseract
-from PIL import Image
-import io
+from fastapi import FastAPI, Query
+from youtube_transcript_api import YouTubeTranscriptApi
+
 
 app = FastAPI()
 
-@app.post("/ocr", response_class=PlainTextResponse)
-async def ocr_image(request: Request):
-    contents = await request.body()
-    image = Image.open(io.BytesIO(contents))
-    text = pytesseract.image_to_string(image)
-    return text
+
+@app.get("/scrape")
+async def scrape_text(youtube_url: str = Query(..., description="YouTube video URL")):
+    ytt_api = YouTubeTranscriptApi()
+    print("reached")
+    video_id = youtube_url.split("v=")[-1].split("&")[0]
+    print(video_id)
+    transcript_dicts = ytt_api.fetch(video_id).to_raw_data()
+
+    to_return = [
+        {
+            "text": transcript["text"],
+            "regular_url": f"https://www.youtube.com/watch?v={video_id}",
+            "url_with_timestamp": f"https://www.youtube.com/watch?v={video_id}&t={transcript['start']}s",
+        }
+        for transcript in transcript_dicts
+    ]
+
+    return {"transcripts": to_return}
